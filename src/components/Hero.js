02fadcc35a73c286ad039e471d163bb523560480
Nobel <nobel_lamun@hotmail.com>
2023-01-05 15:56:10 -0330

    testing compressed img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import Image from "../images/Layer1.webp";
+import Image from "../images/Layer12.webp";
 
 const Hero = () => {
   return (
