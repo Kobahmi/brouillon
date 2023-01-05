ea1108ba3d2925d4e3de2126c07334036de2ccf8
Nobel <nobel_lamun@hotmail.com>
2023-01-05 16:27:49 -0330

    LCP test

--- src/components/Automatisation.js
+++ src/components/Automatisation.js
@@ -16,14 +16,15 @@ const Automatisation = () => {
           </span>
         </h1>
         <div className="flex flex-col gap-10 items-center p-10 md:flex-row">
-          <img
-            data-aos="fade-up"
-            data-aos-duration="1000"
-            className="object-cover h-full w-full"
-            src={Image1}
-            alt="travail"
-            loading="lazy"
-          />
+          <div>
+            <img
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              className="object-cover h-full w-full"
+              src={Image1}
+              alt="travail"
+            />
+          </div>
 
           <div className="flex flex-col gap-4 pl-10 py-6">
             <h2 className="flex flex-col">
