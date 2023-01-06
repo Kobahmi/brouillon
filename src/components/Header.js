9e9cbe6c109baa53ba163302e6a8b7492c63a577
Nobel <nobel_lamun@hotmail.com>
2023-01-05 21:25:09 -0330

    switched svg out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby";
 import * as React from "react";
 import Image1 from "../images/Logo.svg";
+import Menu from "../images/menu.svg";
 import { useState } from "react";
 
 const Header = () => {
@@ -24,33 +25,9 @@ const Header = () => {
                 onClick={() => setNavbar(!navbar)}
               >
                 {navbar ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  <p className="text-white font-bold">X</p>
                 ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
+                  <img className="w-6 h-6" src={Menu} alt="menu" />
                 )}
               </button>
             </div>
@@ -62,8 +39,8 @@ const Header = () => {
               navbar ? "block" : "hidden"
             }`}
           >
-            <ul className="items-center justify-center space-y-8 border-b-2 border-neutral-600 pb-10 lg:pb-0 lg:border-b-0 lg:flex lg:space-x-6 lg:space-y-0">
-              <div className="flex flex-col gap-6 items-center justify-center lg:flex-row">
+            <div className="flex flex-col gap-6 items-center justify-center lg:flex-row">
+              <ul className="items-center justify-center text-center space-y-8 border-b-2 border-neutral-600 pb-10 lg:pb-0 lg:border-b-0 lg:flex lg:space-x-6 lg:space-y-0">
                 <li className="text-white hover:text-indigo-200">
                   <Link to="/">PRODUITS</Link>
                 </li>
@@ -90,8 +67,8 @@ const Header = () => {
                     </span>
                   </Link>
                 </li>
-              </div>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="hidden space-x-2 lg:inline-block">
