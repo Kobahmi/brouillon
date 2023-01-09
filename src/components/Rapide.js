dccdf431e7ca51531eadf537d81c0645a61c5cb4
Nobel <nobel_lamun@hotmail.com>
2023-01-09 18:36:34 -0330

    rapide section height changed

--- src/components/Rapide.js
+++ src/components/Rapide.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 const Rapide = () => {
   return (
-    <section className="bg-neutral-900  min-h-[500px]">
+    <section className="bg-neutral-900  ">
       <div className="flex justify-center items-center  h-full">
         <div className="flex flex-col justify-center items-center px-6 text-center lg:text-start lg:flex-row lg:justify-between">
           <div>
