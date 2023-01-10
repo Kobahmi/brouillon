6ee86b764121d91b245632c0a2fcbb162bd111b2
Nobel <nobel_lamun@hotmail.com>
2023-01-10 12:52:14 -0330

    took out parallax

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import AOS from "aos";
-import Rellax from "rellax";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import Header from "./Header";
@@ -9,17 +8,6 @@ const Layout = ({ children }) => {
   useEffect(() => {
     AOS.init();
     AOS.refresh();
-  }, []);
-
-  useEffect(() => {
-    new Rellax(".animate", {
-      speed: -2,
-      center: true,
-      wrapper: null,
-      round: true,
-      vertical: true,
-      horizontal: false,
-    });
   }, []);
 
   return (
