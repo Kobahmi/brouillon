81c7e99d1411aeece75d14a1a1a624638b247aa4
Nobel <nobel_lamun@hotmail.com>
2023-01-12 12:43:47 -0330

    testing no JS again

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,8 @@
 import * as React from "react";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
+
 import Header from "./Header";
 
 const Layout = ({ children }) => {
-  let AOS;
-  useEffect(() => {
-    AOS = require("aos");
-    AOS.init();
-  }, []);
-
-  useEffect(() => {
-    if (AOS) {
-      AOS.refresh();
-    }
-  });
-
   return (
     <>
       <Header />
