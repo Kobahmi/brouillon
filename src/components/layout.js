4878c7bbd1cc79f28ce7c4cf3d3eb9494afb0301
Nobel <nobel_lamun@hotmail.com>
2023-01-12 21:40:09 -0330

    testing no JS again

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,6 @@
 import * as React from "react";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
 
 const Layout = ({ children }) => {
-  let AOS;
-  useEffect(() => {
-    const AOS = require("aos");
-    AOS.init();
-  }, []);
-
-  useEffect(() => {
-    if (AOS) {
-      AOS.refresh();
-    }
-  });
-
   return (
     <>
       <main>{children}</main>
