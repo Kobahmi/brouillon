f54d0a702edc5c55547062150d1c75bdf459cdab
Nobel <nobel_lamun@hotmail.com>
2023-01-16 18:59:39 -0330

    testing preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-preact`,
     `gatsby-plugin-image`,
     "gatsby-plugin-postcss",
     {
