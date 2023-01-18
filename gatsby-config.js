2c93c4c8d13ca77a1f08e257251308d1a44c7bba
Nobel <nobel_lamun@hotmail.com>
2023-01-18 18:39:20 -0330

    took out webpack bundle analyzer v1 for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
-    "gatsby-plugin-webpack-bundle-analyzer",
     `gatsby-plugin-image`,
     "gatsby-plugin-postcss",
     {
